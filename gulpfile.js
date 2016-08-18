82364d4a37528a003b3b1de9dde1ade509c963fb
Alexandr <bumkaka@yandex.ru>
2016-08-18 20:45:51 +0300

    clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-var inject          = require('gulp-inject'),
-    gulp 		    = require('gulp'),
-    browserify 	    = require('browserify'),
-    babelify 	    = require('babelify'),
-    source 		    = require('vinyl-source-stream'),
-    concat 		    = require('gulp-concat'),
-    minify          = require('gulp-minify');
+var inject = require('gulp-inject'),
+    gulp = require('gulp'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    source = require('vinyl-source-stream'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify');
 
 
 
 var jsLibs = [
-  './node_modules/jquery/dist/jquery.js',
-  './node_modules/react/dist/react-with-addons.js',
-  './node_modules/react-dom/dist/react-dom.min.js'
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/react/dist/react-with-addons.js',
+    './node_modules/react-dom/dist/react-dom.min.js'
 ];
 var cssLibs = [
-  './node_modules/bootstrap/dist/css/bootstrap.css'
+    './node_modules/bootstrap/dist/css/bootstrap.css'
 ];
 
 
@@ -31,22 +31,25 @@ gulp.task('libraries', function () {
 
 
 
-
-
 gulp.task('build', function () {
     gulp
         .src('./src/index.html')
         .pipe(gulp.dest('./www/'));
 
-    browserify({entries: './src/app.jsx', extensions: ['.jsx','.js'], debug: false})
-        .transform('babelify', {presets: ['es2015', 'react']})
+    browserify({
+            entries: './src/app.jsx',
+            extensions: ['.jsx', '.js'],
+            debug: false
+        })
+        .transform('babelify', {
+            presets: ['es2015', 'react']
+        })
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('./www/js/'));
 });
 
 
-gulp.task('default', ['build','libraries'], function () {
+gulp.task('default', ['build', 'libraries'], function () {
     gulp.watch(['./src/**/**/*'], ['build']);
 });
-
--- src/controllers/TodoListController.js
+++ src/controllers/TodoListController.js
@@ -1,39 +1,40 @@
 var TodoListController = {
-    add: function( todo ){
+    add: function (todo) {
         var default_todo = {
-            id: this._getID(),
-            title: 'default',
-            done: false
-        }
-        var new_todo = Object.assign(default_todo, todo);
+                id: this.getID(),
+                title: 'default',
+                done: false
+            },
+            new_todo = Object.assign(default_todo, todo),
+            todos = Simplex.todos;
 
-        var todos = Simplex.todos;
-        todos.push( new_todo )
+        todos.push(new_todo);
         Simplex.todos = todos;
     },
 
-    remove: function( id ){
-        var todos = Simplex.todos;
-        var todos = Simplex.todos.filter( ( todo )=>{
+    remove: function (id) {
+        var todos = Simplex.todos.filter((todo) => {
             return todo.id != id;
         });
+
         Simplex.todos = todos;
     },
 
-    updateById: function( id, updated_todo ){
-        var todos = Simplex.todos.map(( todo )=>{
+    updateById: function (id, updated_todo) {
+        var todos = Simplex.todos.map((todo) => {
             return todo.id == id ? updated_todo : todo;
         })
         Simplex.todos = todos;
     },
 
-    get: function( id ){
+    get: function (id) {
 
     },
 
-    _getID(){
-        return 'xxxx-xxxx-xxxx-xxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    getID() {
+        return 'xxxx-xxxx-xxxx-xxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     }
